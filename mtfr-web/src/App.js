4d73f0fcef99aeb0fd9163daf954bde4b37ecec1
Matt Barich <mattbarich1@gmail.com>
2025-02-12 03:19:52 +0000

    Done for day... Nav Bar almost done - 17:25

--- mtfr-web/src/App.js
+++ mtfr-web/src/App.js
@@ -1,11 +1,14 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout'
+
 function App() {
   return (
+    <>
     <Routes>
-      <Route path="/" element={<layout />} />
+      <Route path="/" element={<Layout />} />
     </Routes>
+    </>
   );
 }
 
